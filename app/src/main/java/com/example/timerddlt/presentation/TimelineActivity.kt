df6addbfae16811bb3c0fe9846041f81b190a29d
luanle912 <luanle912@hcmut.edu.vn>
2022-05-22 00:48:37 +0700

    fix activity

---
+++ app/src/main/java/com/example/timerddlt/presentation/TimelineActivity.kt
@@ -0,0 +1,11 @@
+package com.example.timerddlt.presentation
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.timerddlt.R
+
+class TimelineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timeline)
+    }
+}
