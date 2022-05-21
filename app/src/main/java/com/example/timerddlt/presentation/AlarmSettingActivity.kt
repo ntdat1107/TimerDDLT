554ef47f265edd7de17dd069f24ddacb8b81c239
TienMinh <tienminh0801@gmail.com>
2022-05-22 02:00:02 +0700

    setting, alarm_setting, statistics UI

---
+++ app/src/main/java/com/example/timerddlt/presentation/AlarmSettingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.timerddlt.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.timerddlt.R
+
+class AlarmSettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alarm_setting)
+    }
+}
---
+++ app/src/main/java/com/example/timerddlt/presentation/StastisticsMonthActivity.kt
@@ -0,0 +1,12 @@
+package com.example.timerddlt.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.timerddlt.R
+
+class StastisticsMonthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stastistics_month)
+    }
+}
