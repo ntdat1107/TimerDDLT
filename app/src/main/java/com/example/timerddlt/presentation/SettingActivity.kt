339bbc862c3f48b1b9d65c0de5cf390890d0da4c
TienMinh <tienminh0801@gmail.com>
2022-05-24 16:14:29 +0700

    Fix conflict

--- app/src/main/java/com/example/timerddlt/presentation/SettingActivity.kt
+++ app/src/main/java/com/example/timerddlt/presentation/SettingActivity.kt
@@ -7,6 +7,6 @@ import com.example.timerddlt.R
 class SettingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting2)
+        setContentView(R.layout.activity_setting)
     }
 }
