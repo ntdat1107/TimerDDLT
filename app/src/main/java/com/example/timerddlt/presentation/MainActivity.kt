b46db95f4f8656b05e319b58ffa8f114f9005730
luanle912 <luanle912@hcmut.edu.vn>
2022-05-29 01:03:34 +0700

    play music

--- app/src/main/java/com/example/timerddlt/presentation/MainActivity.kt
+++ app/src/main/java/com/example/timerddlt/presentation/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.timerddlt.presentation
 
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
@@ -12,6 +15,8 @@ import androidx.drawerlayout.widget.DrawerLayout
 import com.example.timerddlt.R
 import com.example.timerddlt.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
+import android.widget.MediaController.MediaPlayerControl
+
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -19,10 +24,22 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navigationView: NavigationView
 
+//  music
+    private lateinit var mp: MediaPlayer;
+    private var totalTime: Int = 0;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+
+        // music
+        mp = MediaPlayer.create(this, R.raw.phutbandau)
+        mp.isLooping = true
+//        mp.setVolume(0.5f, 0.5f)
+        mp.start()
+        totalTime = mp.duration
 
         drawerLayout = binding?.drawerLayout!!
         navigationView = binding?.navView!!
@@ -66,4 +83,24 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         drawerLayout.close()
         return true
     }
+
+    //music
+    fun playBtnClick(v: View){
+        if(mp.isPlaying){
+            //Stop
+            mp.pause()
+            var button : ImageView = findViewById(R.id.iv_music)
+            button.setImageResource(R.drawable.ic_music_off)
+        }
+        else{
+            //Start
+            mp.start()
+            var button : ImageView = findViewById(R.id.iv_music)
+            button.setImageResource(R.drawable.ic_music_on)
+        }
+    }
 }
+
+
+
+
