0a813ca88e01ebc5d8a52e4aadd30cda8248ae2f
Jinnux <nguyentandat26032001@gmail.com>
2022-05-31 02:24:44 +0700

    temp

--- app/src/main/java/com/example/timerddlt/data/data_source/TimerDatabase.kt
+++ app/src/main/java/com/example/timerddlt/data/data_source/TimerDatabase.kt
@@ -14,6 +14,7 @@ import com.example.timerddlt.domain.model.NextEvent
 )
 abstract class TimerDatabase : RoomDatabase() {
     abstract fun useTimerDao() : TimerDao
+    abstract fun useNextEventDao() : NextEventDao
 
     companion object {
         private const val DATABASE_NAME = "timer_db"
