fee8fa6fe0ee6151f9d4d74fc653891372010b60
Jinnux <nguyentandat26032001@gmail.com>
2022-05-30 12:58:06 +0700

    easy fix

--- app/src/main/java/com/example/timerddlt/data/repository/TimerRepositoryImpl.kt
+++ app/src/main/java/com/example/timerddlt/data/repository/TimerRepositoryImpl.kt
@@ -1,28 +1,27 @@
 package com.example.timerddlt.data.repository
 
-import android.content.Context
 import com.example.timerddlt.data.data_source.TimerDao
-import com.example.timerddlt.data.data_source.TimerDatabase
 import com.example.timerddlt.domain.model.Event
 import com.example.timerddlt.domain.repository.TimerRepository
 import kotlinx.coroutines.flow.Flow
 
-class TimerRepositoryImpl (context: Context) : TimerRepository {
-    val timerDao : TimerDao = TimerDatabase.provideNoteDatabase(context).useTimerDao()
+class TimerRepositoryImpl (
+    private val timerDao : TimerDao
+) : TimerRepository {
 
     override fun getEvents(): Flow<List<Event>> {
-        TODO("Not yet implemented")
+        return timerDao.getEvents()
     }
 
     override suspend fun getEventById(id: Int): Event? {
-        TODO("Not yet implemented")
+        return timerDao.getEventById(id)
     }
 
     override suspend fun insertEvent(event: Event) {
-        TODO("Not yet implemented")
+        timerDao.insertEvent(event)
     }
 
     override suspend fun deleteEvent(event: Event) {
-        TODO("Not yet implemented")
+        timerDao.deleteEvent(event)
     }
 }
