602e8e62179eed3279802032c56a2db7c01b7e2f
Jinnux <nguyentandat26032001@gmail.com>
2022-05-30 12:51:42 +0700

    temp

---
+++ app/src/main/java/com/example/timerddlt/services/BroadcastService.kt
@@ -0,0 +1,52 @@
+package com.example.timerddlt.services
+
+import android.app.Service
+import android.content.Intent
+import android.os.CountDownTimer
+import android.os.IBinder
+import android.util.Log
+
+class BroadcastService : Service() {
+    companion object {
+        const val COUNTDOWN_BR = "com.example.timerddlt.services"
+    }
+
+    val intent = Intent(COUNTDOWN_BR)
+
+    private var mCountDownTimer: CountDownTimer? = null
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        val prefs = getSharedPreferences("prefs", MODE_PRIVATE)
+        val remainingTime: Long = prefs.getLong("remainingTimeInMillis", -1)
+        mCountDownTimer = object : CountDownTimer(remainingTime + 2000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                intent.putExtra("countdown", millisUntilFinished - 2000)
+                sendBroadcast(intent)
+            }
+
+            override fun onFinish() {
+                Log.i("testOnTick", "0")
+                intent.putExtra("countdown", 0)
+                sendBroadcast(intent)
+                intent.putExtra("finish", 1)
+                sendBroadcast(intent)
+            }
+
+        }.start()
+    }
+
+    override fun onDestroy() {
+        mCountDownTimer!!.cancel()
+        super.onDestroy()
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+}
