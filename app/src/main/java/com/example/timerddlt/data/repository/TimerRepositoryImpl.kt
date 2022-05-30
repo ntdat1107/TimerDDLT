ffde855cce8e22cd3a67224f39b12620955fa3e9
Jinnux <nguyentandat26032001@gmail.com>
2022-05-30 21:24:19 +0700

    ok

--- app/src/main/java/com/example/timerddlt/data/repository/TimerRepositoryImpl.kt
+++ app/src/main/java/com/example/timerddlt/data/repository/TimerRepositoryImpl.kt
@@ -1,4 +1,5 @@
 package com.example.timerddlt.data.repository
+<<<<<<< HEAD
 
 import android.content.Context
 import android.util.Log
@@ -36,4 +37,30 @@ class TimerRepositoryImpl (
         }
     }
 
+=======
+import com.example.timerddlt.data.data_source.TimerDao
+import com.example.timerddlt.domain.model.Event
+import com.example.timerddlt.domain.repository.TimerRepository
+import kotlinx.coroutines.flow.Flow
+
+class TimerRepositoryImpl (
+    private val timerDao : TimerDao
+    ) : TimerRepository {
+
+    override fun getEvents(): Flow<List<Event>> {
+        return timerDao.getEvents()
+    }
+
+    override suspend fun getEventById(id: Int): Event? {
+        return timerDao.getEventById(id)
+    }
+
+    override suspend fun insertEvent(event: Event) {
+        timerDao.insertEvent(event)
+    }
+
+    override suspend fun deleteEvent(event: Event) {
+        timerDao.deleteEvent(event)
+    }
+>>>>>>> tiendat
 }
--- app/src/main/java/com/example/timerddlt/presentation/FinishActivity.kt
+++ app/src/main/java/com/example/timerddlt/presentation/FinishActivity.kt
@@ -1,12 +1,33 @@
 package com.example.timerddlt.presentation
 
+<<<<<<< HEAD
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+=======
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+>>>>>>> tiendat
 import com.example.timerddlt.R
 
 class FinishActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
+<<<<<<< HEAD
+=======
+
+        val prefs = getSharedPreferences("prefs", MODE_PRIVATE)
+        val editor = prefs.edit()
+        editor.putInt("timerRunning", 0)
+        editor.apply()
+
+        val isSuccess = intent.getBooleanExtra("isSuccess", true)
+        if (!isSuccess) {
+
+        } else {
+
+        }
+
+>>>>>>> tiendat
     }
 }
