f454db92da2cef16f653500d86eaa2856c3cf038
Jinnux <nguyentandat26032001@gmail.com>
2022-05-30 21:28:11 +0700

    Revert "ok"
    This reverts commit ffde855cce8e22cd3a67224f39b12620955fa3e9.

--- app/src/main/java/com/example/timerddlt/domain/model/Event.kt
+++ app/src/main/java/com/example/timerddlt/domain/model/Event.kt
@@ -1,22 +1,12 @@
 package com.example.timerddlt.domain.model
-import android.text.Editable
 import androidx.room.*
 
 @Entity
 data class Event(
-<<<<<<< HEAD
     val title: String,
     val description: String,
     val lasting: Long,
     val startTime: Long,
     val endTime: Long,
-=======
-    var title: String,
-    var description: String,
-    val lasting: Long,
-    val startTime: Long,
-    val endTime: Long,
-    val isSuccess: Boolean,
->>>>>>> tiendat
     @PrimaryKey val id: Int? = null
 )
